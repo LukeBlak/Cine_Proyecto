ac30d9d1b894cc5de7382fce469cebe6d0ffbf06
RobertoMilan <88417662+Robertogame666@users.noreply.github.com>
2025-11-19 23:38:05 -0600

    Update panel.js

--- JS/panel.js
+++ JS/panel.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('userRole').textContent = userRole === 'admin' ? 'Administrador General' : 'Empleado';
     
     // Mostrar sección de admin solo si es admin
-    if (role === 'admin') {
+    if (userRole === 'admin') {
         document.getElementById('adminSection').style.display = 'block';
     }
 
